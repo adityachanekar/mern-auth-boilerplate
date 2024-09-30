a288ebb9b43518b205dec85a6985c017264611bc
Aditya Chanekar <chanekar.aditya@gmail.com>
2024-09-30 19:19:45 +0530

    Added db config and basic regristration in backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express')
-//const connectDB = require('./config/db')
+const connectDB = require('./config/db')
 const app = express();
+console.debug(process.env.PORT)
 const PORT = process.env.PORT || 8086;
 
 // Connect Database
-//connectDB();
+connectDB();
 
 app.use(express.urlencoded())
 app.use(express.json())
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
   res.send(`API hit server -- `);
 });
 
-// app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/transactions', require('./routes/transactions'));
 
 app.listen(PORT, () => {
---
+++ server/routes/auth.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Register User
+router.post('/register', async (req, res) => {
+    console.log('body -> ',req.body)
+  const { name, email, password } = req.body;
+
+  try {
+    let user = email != null ? await User.findOne({ email }) : null;
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+router.get('/userLog', async (req,res) =>{
+    console.info(`api/userLog hit by -- ${req.ip}`)
+    try{
+        let userTable = await User.find({});
+        if (userTable) res.status(200).send(userTable);
+        else res.status(201).send({ msg: 'DB Error' });
+
+        console.table(JSON.stringify(userTable,true, 2))
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+})
+// Login User
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
